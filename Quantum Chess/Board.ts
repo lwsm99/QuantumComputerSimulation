3fa0fc6cec53d2e64165e1d71b3e9500cc10e8ab
Lucas <lucasweissmann@gmx.de>
2023-01-12 20:09:09 +0100

    Quantum Chess: Fix resetBoard

--- Quantum Chess/Board.ts
+++ Quantum Chess/Board.ts
@@ -1,4 +1,8 @@
 import { Spot } from "./Spot"
+import { Pawn } from "./Pieces/Pawn"
+import { Queen } from "./Pieces/Queen"
+import { Knight } from "./Pieces/Knight"
+import { Bishop } from "./Pieces/Bishop"
 import { King } from "./Pieces/King"
 import { Rook } from "./Pieces/Rook"
 
@@ -6,21 +10,25 @@ export class Board {
     occupation: boolean[] // Qubits that store the occupation of a tile (1 if non-zero probability)
     tiles: Spot[] // Bits that stores which piece occupies a tile
 
-    public resetBoard() {
+    public resetBoard(): void {
         const pieceOrder = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
-    
-        this.tiles = this.tiles.map((_, index) => {
-            if (index < 8) {
-                return new Spot(new pieceOrder[index](true), index)
-            } else if (index < 16) {
-                return new Spot(new Pawn(true), index)
-            } else if (index >= 48) {
-                return new Spot(new pieceOrder[index - 48](false), index)
-            } else if (index >= 40) {
-                return new Spot(new Pawn(false), index)
+        
+        // Initialize Chess Board  
+        this.tiles = []
+        this.occupation = []
+        for (let i = 0; i < 64; i++) {
+            if (i < 8) {
+                this.tiles[i] = new Spot(new pieceOrder[i](true), i)
+            } else if (i < 16) {
+                this.tiles[i] = new Spot(new Pawn(true), i)
+            } else if (i >= 56) {
+                this.tiles[i] = new Spot(new pieceOrder[i - 56](false), i)
+            } else if (i >= 48) {
+                this.tiles[i] = new Spot(new Pawn(false), i)
             } else {
-                return new Spot(null, index)
+                this.tiles[i] = new Spot(null, i)
             }
-        });
+            this.occupation.push(false)
+        }
     }
 }
